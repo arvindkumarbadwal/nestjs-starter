3cf0c22d1a4cc0d2bddc873dfae0098f9b249fbb
Arvindkumar Badwal <arvindkumar.badwal@ambab.com>
2021-04-22 15:04:41 +0530

    dashboard and auth page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -15,4 +15,9 @@ export class AppController {
   login(@Res() res: Response) {
     return res.render('admin/auth/login')
   }
+
+  @Get('dashboard')
+  dashboard(@Res() res: Response) {
+    return res.render('admin/dashboard/index')
+  }
 }
